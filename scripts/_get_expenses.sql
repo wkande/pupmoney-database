0a4b3f1e20f75940bde85eea5efe43a932ca5919
Warren Anderson <warren@Warrens-Mac-mini.local>
2019-04-22 14:09:17 -0600

    minor changes to output messages during data creation

--- scripts/_get_expenses.sql
+++ scripts/_get_expenses.sql
@@ -1,9 +1,9 @@
 /**
- * Gets a summary list of expenses within a date range and an optional text search.
+ * Gets a list of expenses within a date range and an optional text search.
  */
 
 
-CREATE OR REPLACE FUNCTION get_expenses(q TEXT, dttmStart DATE, dttmEnd DATE, walletId integer, catID integer, skip integer)
+CREATE OR REPLACE FUNCTION get_expenses(q TEXT, dttmStart DATE, dttmEnd DATE, walletId integer, skip integer)
 
     RETURNS items AS $$
     DECLARE
@@ -14,34 +14,34 @@ CREATE OR REPLACE FUNCTION get_expenses(q TEXT, dttmStart DATE, dttmEnd DATE, wa
     IF length(q) = 0 THEN
             SELECT array_to_json(array_agg(row_to_json(t))) INTO result.items 
             from (  
-                SELECT e.id, e.category_id, c.wallet_id, e.document, e.amt, e.dttm, e.note, e.vendor
+                SELECT e.id, e.category_id, c.id c_id, c.name c_name, c.wallet_id, e.document, e.amt, e.dttm, e.note, e.vendor
                 FROM expenses e JOIN categories c
                 ON e.category_id = c.id
-                WHERE e.dttm between dttmStart AND dttmEnd AND c.wallet_id = walletId AND e.category_id = catId 
-                ORDER BY e.dttm DESC LIMIT 50 OFFSET skip
+                WHERE e.dttm between dttmStart AND dttmEnd AND c.wallet_id = walletId  
+                ORDER BY e.dttm DESC, e.id DESC LIMIT 50 OFFSET skip
             ) t;
 
             SELECT count(*) cnt into result.total_cnt 
             FROM expenses e JOIN categories c
             ON e.category_id = c.id
-            WHERE e.dttm between dttmStart AND dttmEnd AND c.wallet_id = walletId AND e.category_id = catID;
+            WHERE e.dttm between dttmStart AND dttmEnd AND c.wallet_id = walletId;
 
         ELSE
             SELECT array_to_json(array_agg(row_to_json(t))) INTO result.items 
             from (  
-                SELECT e.id, e.category_id, c.wallet_id, e.document, e.amt, e.dttm, e.note, e.vendor
+                SELECT e.id, e.category_id, c.id c_id, c.name c_name, c.wallet_id, e.document, e.amt, e.dttm, e.note, e.vendor
                 FROM expenses e JOIN categories c
                 ON e.category_id = c.id
                 WHERE e.document @@ to_tsquery(q)
-                AND e.dttm between dttmStart AND dttmEnd AND c.wallet_id = walletId AND e.category_id = catId 
-                ORDER BY e.dttm DESC LIMIT 50 OFFSET skip
+                AND e.dttm between dttmStart AND dttmEnd AND c.wallet_id = walletId 
+                ORDER BY e.dttm DESC, e.id DESC LIMIT 50 OFFSET skip
             ) t;
 
             SELECT count(*) cnt into result.total_cnt 
             FROM expenses e JOIN categories c
             ON e.category_id = c.id
             WHERE e.document @@ to_tsquery(q)
-            AND e.dttm between dttmStart AND dttmEnd AND c.wallet_id = walletId AND e.category_id = catID;
+            AND e.dttm between dttmStart AND dttmEnd AND c.wallet_id = walletId;
 
         END IF;
 
